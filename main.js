bd1001b39ef2e4f34621712a12e11742f1f8056d
sarthak966 <113447932+sarthak966@users.noreply.github.com>
2023-04-26 16:47:37 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
-let menuButton=document.getElementById("cat1");
+let btn=document.getElementsByClassName("open")[0];
 let menu=document.getElementById("menu_ul");
+let btn2=document.getElementsByClassName("close")[0];
 
-menu.style.right="-1000px"
+if(window.innerWidth<=900){
+btn2.style.display="none";
+menu.style.display="none";
 
-menuButton.addEventListener("click",()=>{
+btn.addEventListener("click",()=>{
+    menu.style="display:flex;width:75vw;";
+    menu.classList.add("menu_ani1");
+    btn.style.display="none";
+    btn2.style.display="block";
 
-    if(menu.style.right==="-1000px"){
-        menu.style="right:0px;"
-    }
-    else if(menu.style.right!="-1000px"){
-        menu.style="right:-1000px;"
-    }
-    })
+})
 
-console.log(menu)
+btn2.addEventListener("click",()=>{
+    
+    
+    
+        menu.classList.remove("menu_ani1");
+        // menu.classList.add("menu_ani2");
+    btn2.style.display="none";
+    btn.style.display="block";
+    
+    menu.style.display="none";
+    
+
+})
+}
+console.log(document.documentElement.clientWidth)
+
+
